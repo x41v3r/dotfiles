04b0e8e82469ee0dab73b548e7948ff5fee308ea
x41v3r <x41v3ryang@gmail.com>
2025-01-11 11:02:58 +0800

    transfer

--- .config/nvim/lua/plugins/nvim-widget.lua
+++ .config/nvim/lua/plugins/nvim-widget.lua
@@ -1,22 +1,22 @@
 return {
 	-- nvim tree file manager
-	{
-		'nvim-tree/nvim-tree.lua',
-		dependencies = {
-			'nvim-tree/nvim-web-devicons'
-		},
-		config = function()
-			require("nvim-tree").setup{}
-		end,
-	},
+	--{
+	--	'nvim-tree/nvim-tree.lua',
+	--	dependencies = {
+	--		'nvim-tree/nvim-web-devicons'
+	--	},
+	--	config = function()
+	--		require("nvim-tree").setup{}
+	--	end,
+	--},
 	-- nvim terminal
-	{
-		"s1n7ax/nvim-terminal",
-		    config = function()
-			    vim.o.hidden = true
-			    require('nvim-terminal').setup()
-		    end,
-	},
+	--{
+	--	"s1n7ax/nvim-terminal",
+	--	    config = function()
+	--		    vim.o.hidden = true
+	--		    require('nvim-terminal').setup()
+	--	    end,
+	--},
 	
 	-- bufferline
 	--{
